03ccc1cd517e3ffb49f503708189d641489da890
akshaya1909 <akshayanatalia356@gmail.com>
2024-11-13 19:51:35 +0530

    Till Folder 12 II

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,6 @@ app.use(express.urlencoded({ extended: true }));
 //cookie parser middleware
 app.use(cookieParser());
 
-// (URL/ API endpoints, (req, res as arrow function))
-app.get('/', (req, res) => {
-res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -42,6 +37,22 @@ app.get('/api/config/paypal', (req, res) => res.send({
 
 const __dirname = path.resolve(); // Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    // any route that is not api will be redirected to index.html
+    app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+)
+}
+else{
+    // (URL/ API endpoints, (req, res as arrow function))
+    app.get('/', (req, res) => {
+    res.send('API is running...');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
